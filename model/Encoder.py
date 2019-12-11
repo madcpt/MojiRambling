b2acfcb6759212667a2d67f18aeaf2fb23ef899c
madcpt <zihan.xu@sjtu.edu.cn>
2019-12-11 21:35:03 +0800

    add drop-out

--- model/Encoder.py
+++ model/Encoder.py
@@ -10,13 +10,14 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
         self.vocab_size = vocab_size
         self.encoder = nn.GRU(input_size=400, hidden_size=400, num_layers=2, batch_first=True)
+        self.dropout_layer = nn.Dropout(0.1)
 
     def forward(self, input_embedding, input_lens, hidden=None):
         input_lens = np.array(input_lens)
         sort_idx = np.argsort(-input_lens)
         input_lengths = input_lens[sort_idx]
         sort_input_seq = input_embedding[sort_idx]
-        embedded = sort_input_seq  # 现在 embedding 层统一了，就放在主模型
+        embedded = self.dropout_layer(sort_input_seq)   # 现在 embedding 层统一了，就放在主模型
         packed = torch.nn.utils.rnn.pack_padded_sequence(embedded, input_lengths, batch_first=True)
         outputs, en_hidden = self.encoder(packed, hidden)
         outputs, _ = torch.nn.utils.rnn.pad_packed_sequence(outputs, batch_first=True)
